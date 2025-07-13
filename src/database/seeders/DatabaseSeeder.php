8a5f4d853f843af2b9e2e6c9d6796636d096f270
Antônio Neto <acm.neto1999@gmail.com>
2025-07-13 00:10:49 -0300

    :sparkles: feat: adicionando chamada dos seeders criados no DatabaseSeeder

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+class DatabaseSeeder extends Seeder {
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+    public function run(): void {
+        $this->call([
+            CidadeSeeder::class,
+            ClienteSeeder::class,
+            RepresentanteSeeder::class,
         ]);
     }
 }
